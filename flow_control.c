928d26e688e6d07278c4f53b2e645c3887c2876f
Kyle Brewington <kbrewington@smcm.edu>
2018-04-11 10:56:36 -0400

    Added basic code to frame_arrived

--- flow_control.c
+++ flow_control.c
@@ -26,6 +26,11 @@ CnetTimerID retransmit_timer;
 
 EVENT_HANDLER(send_frame) {
   // TODO Implement transmitting a frame
+
+  // read application to send buffer
+  // send buffer sequence number = transmitted
+  // write physical
+  // disable application
 }
 
 EVENT_HANDLER(retransmit_timer_handler) {
@@ -34,10 +39,81 @@ EVENT_HANDLER(retransmit_timer_handler) {
 
 EVENT_HANDLER(frame_arrived) {
   // TODO Implement receiving a frame
+
+  // generate CRC for new package
+  // check new CRC with sent CRC
+  // if CRC matches, check with expected if it is the expected frame
+  // if CRC, and expected match, send ACKNOWLEGMENT
+
+  // ???? length = buffer.header.length + sizeof( SegmentHeader );
+
+  Segment buffer;
+
+  CHECK(
+    CNET_read_physical(
+      1, &buffer, &buffer.length
+    )
+  );
+
+  if (buffer.length == 0) {
+    // TODO check buffer.sequence_number to transmitted
+      // if true, transmitted ++
+      // send new packet
+      // reset resend_ack timer
+
+    if (buffer.sequence_number == transmitted) {
+      transmitted++;
+
+      // Get new packet
+      CHECK(
+        CNET_read_application(
+          &destination, &send_buffer, &(send_buffer.header.length)
+        )
+      );
+
+      send_buffer.sequence_number == transmitted;
+
+    }
+  }
+
+  // Generate a new CRC code on the sent pack
+  // Check the generated CRC code against the CRC sent with package
+  if (buffer.crc == CNET_crc32(&ack_buffer, buffer.header.length + sizeof( SegmentHeader ))) {
+
+    // Check to see if package is expected
+    if (buffer.sequence_number == expected) {
+      expected++;
+
+      ack_buffer.sequence_number = buffer.sequence_number;
+      // set to 0 cause we're not sending data
+      ack_buffer.header.length = 0;
+
+      // TODO Write to application
+
+      // TODO Send ACKNOWLEGMENT
+      CHECK(
+        CNET_write_physical(
+          1, &ack_buffer, &length
+        )
+      );
+
+      // TODO Reset reacknowledge_timer_handler
+    }
+    else {
+      printf("ERROR: Sequence_number %d received. Expected %d.\n", ack_buffer.sequence_number, expected);
+    }
+  }
+  else {
+    printf("ERROR: CRC check failed.\n");
+  }
 }
 
 EVENT_HANDLER(reacknowledge_timer_handler) {
   // TODO Implement the reacknowledgement timer going off
+
+  // TODO Send ACKNOWLEGMENT
+
+  // TODO Reset reacknowledge_timer_handler
 }
 
 EVENT_HANDLER(reboot_node) {
